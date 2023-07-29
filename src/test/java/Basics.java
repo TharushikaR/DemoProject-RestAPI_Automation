dbc9dbf2ef63d94149471280e41d2f88d8d40a40
Tharushi <t.rathnayaka1995@gmail.com>
2023-07-29 14:10:19 +0530

    Initial commit

---
+++ src/test/java/Basics.java
@@ -0,0 +1,11 @@
+public class Basics {
+    public static void main(String[] args){
+        //Validate if Add Place API is working as expected
+
+        //given - all input details
+        //when - submit the API
+        //Then - validate the response
+
+        RestA
+    }
+}
